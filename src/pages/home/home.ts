98aba4db9a6375b697e29faabc7d543e5ff45d62
TakahiroSono <taka.sono889@gmail.com>
2018-10-20 22:19:57 +0900

    add style navbar

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { UserStatusComponent } from '../../components/user-status/user-status';
 
 @Component({
   selector: 'page-home',
